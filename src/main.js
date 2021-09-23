60ef8f88894c463eb4076c78808c397564dc0ac0
周舟 <1416668406@qq.com>
2021-09-23 10:17:03 +0800

    '初始化代码'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// 加载全局样式  【增加这一句】
+import './styles/index.less'
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
